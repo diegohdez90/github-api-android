738d657ba6b6d79c6a585301b659f49ec35dc5c3
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-09-07 10:33:56 -0500

    chore(app): add AndroidNetworking library
    init AndroidNetworking in main activity application

--- app/src/main/java/code/diegohdez/githubapijava/GithubApi.java
+++ app/src/main/java/code/diegohdez/githubapijava/GithubApi.java
@@ -2,9 +2,14 @@ package code.diegohdez.githubapijava;
 
 import android.app.Application;
 
+import com.androidnetworking.AndroidNetworking;
+import com.jacksonandroidnetworking.JacksonParserFactory;
+
 public class GithubApi extends Application{
     @Override
     public void onCreate() {
         super.onCreate();
+        AndroidNetworking.initialize(getApplicationContext());
+        AndroidNetworking.setParserFactory(new JacksonParserFactory());
     }
 }
