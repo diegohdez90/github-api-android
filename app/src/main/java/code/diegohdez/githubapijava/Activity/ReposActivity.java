c2d30280c08af89240e196c10be4ea624661be26
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-09-07 14:41:41 -0500

    feat(ReposActivity): init activity

---
+++ app/src/main/java/code/diegohdez/githubapijava/Activity/ReposActivity.java
@@ -0,0 +1,13 @@
+package code.diegohdez.githubapijava.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReposActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_repos);
+    }
+}
