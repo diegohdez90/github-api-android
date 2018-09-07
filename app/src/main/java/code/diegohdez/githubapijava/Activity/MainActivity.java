cbc43597ee539d50ea7f7ca36967cb248574704c
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-09-07 12:15:13 -0500

    feat(MainActivity): add options item selected to provide onclick event for get token item menu

--- app/src/main/java/code/diegohdez/githubapijava/Activity/MainActivity.java
+++ app/src/main/java/code/diegohdez/githubapijava/Activity/MainActivity.java
@@ -5,8 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.androidnetworking.AndroidNetworking;
 import com.androidnetworking.common.ANRequest;
@@ -70,5 +72,16 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.login_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.openGetToken:
+                Toast.makeText(getApplicationContext(), "Get Token", Toast.LENGTH_SHORT).show();
+                return true;
+                default:
+                    return super.onOptionsItemSelected(item);
+        }
     }
 }
