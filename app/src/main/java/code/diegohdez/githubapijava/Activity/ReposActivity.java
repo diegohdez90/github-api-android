6d2e8659d149cc5ce081bff64810c0af3d22c929
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-09-07 18:50:33 -0500

    feat(Repos): delete realm instances where app is destryed or paused

--- app/src/main/java/code/diegohdez/githubapijava/Activity/ReposActivity.java
+++ app/src/main/java/code/diegohdez/githubapijava/Activity/ReposActivity.java
@@ -1,13 +1,30 @@
 package code.diegohdez.githubapijava.Activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import code.diegohdez.githubapijava.R;
+import io.realm.Realm;
 
 public class ReposActivity extends AppCompatActivity {
 
+    private Realm realm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_repos);
+        realm = Realm.getDefaultInstance();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        realm.close();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        realm.close();
     }
 }
