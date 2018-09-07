62934e1f7e15a9d1ed75395c32c3599d9d210b40
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-09-07 18:49:54 -0500

    chore(GetToken): delete unnecessary import library

--- app/src/main/java/code/diegohdez/githubapijava/Activity/GetToken.java
+++ app/src/main/java/code/diegohdez/githubapijava/Activity/GetToken.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
