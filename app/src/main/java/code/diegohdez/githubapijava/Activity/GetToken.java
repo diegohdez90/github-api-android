9f228ebd692fd153f6674be6e84e42d1d4a1dcab
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-09-07 18:47:37 -0500

    chore(Result): init onResult constant values

--- app/src/main/java/code/diegohdez/githubapijava/Activity/GetToken.java
+++ app/src/main/java/code/diegohdez/githubapijava/Activity/GetToken.java
@@ -1,5 +1,6 @@
 package code.diegohdez.githubapijava.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
@@ -8,7 +9,18 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.ANRequest;
+import com.androidnetworking.common.ANResponse;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+
+import code.diegohdez.githubapijava.Manager.AppManager;
 import code.diegohdez.githubapijava.R;
+
+import static code.diegohdez.githubapijava.Utils.Constants.API.BASE_URL;
+import static code.diegohdez.githubapijava.Utils.Constants.API.USER;
+import static code.diegohdez.githubapijava.Utils.Constants.Result.RESULT_OK_GET_TOKEN;
 
 public class GetToken extends AppCompatActivity {
 
@@ -32,11 +44,32 @@ public class GetToken extends AppCompatActivity {
         if (username.length() > 0 && password.length() > 0) {
             String encode = "";
             try {
-                encode = android.util.Base64.encodeToString((username + ":" + username).getBytes(), Base64.DEFAULT);
+                encode = android.util.Base64.encodeToString((username + ":" + password).getBytes("UTF-8"), Base64.DEFAULT);
             } catch (Exception e) {
                 e.printStackTrace();
             }
-            Log.i(TAG, "token: "+ encode);
+            ANRequest request =  AndroidNetworking
+                    .get(BASE_URL + USER)
+                    .addHeaders("Authorization", "Basic " +  encode)
+                    .setPriority(Priority.HIGH)
+                    .build();
+            ANResponse response = request.executeForJSONObject();
+            if (response.isSuccess()) {
+                AppManager appManager = AppManager.getOurInstance();
+                appManager.setToken(encode.toString());
+                Intent intent = new Intent();
+                setResult(RESULT_OK_GET_TOKEN, intent);
+                finish();
+            } else {
+                ANError error = response.getError();
+                Toast.makeText(getApplicationContext(),
+                        "Error: " + error.getErrorDetail() + "\n" +
+                                "Body: " + error.getErrorBody() + "\n" +
+                                "Message: " + error.getMessage() +  "\n" +
+                                "Code: " + error.getErrorCode(),
+                        Toast.LENGTH_SHORT)
+                        .show();
+            }
         } else {
             Toast.makeText(
                     getApplicationContext(),
---
+++ app/src/main/java/code/diegohdez/githubapijava/Utils/Constants/Result.java
@@ -0,0 +1,6 @@
+package code.diegohdez.githubapijava.Utils.Constants;
+
+public class Result {
+    public static final int RESULT_MAIN_GET_TOKEN = 1001;
+    public static final int RESULT_OK_GET_TOKEN = 1002;
+}
