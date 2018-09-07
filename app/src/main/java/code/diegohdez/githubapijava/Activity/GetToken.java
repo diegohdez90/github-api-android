10bd973b0291b027678b260c5a2d423747fe56ac
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-09-07 12:16:29 -0500

    feat(GetToken): add activity to provide user a token to use app

---
+++ app/src/main/java/code/diegohdez/githubapijava/Activity/GetToken.java
@@ -0,0 +1,15 @@
+package code.diegohdez.githubapijava.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import code.diegohdez.githubapijava.R;
+
+public class GetToken extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_token);
+    }
+}
