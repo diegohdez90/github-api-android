dd18a077e5c10eb7ff6f25a785c6cd0be69d7a7e
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-09-07 09:47:27 -0500

    chore: init app
    add init file to launch application

---
+++ app/src/main/java/code/diegohdez/githubapijava/GithubApi.java
@@ -0,0 +1,10 @@
+package code.diegohdez.githubapijava;
+
+import android.app.Application;
+
+public class GithubApi extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
