4e358e87cddb26d43598b3b0a585123f25a5eed1
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-09-07 11:19:47 -0500

    chore(API): update BASE_URL constant

---
+++ app/src/main/java/code/diegohdez/githubapijava/Utils/Constants/API.java
@@ -0,0 +1,7 @@
+package code.diegohdez.githubapijava.Utils.Constants;
+
+public class API {
+    public static final String BASE_URL = "https://api.github.com/";
+    public static final String USERS = "users/";
+    public static final String USER = "user";
+}
