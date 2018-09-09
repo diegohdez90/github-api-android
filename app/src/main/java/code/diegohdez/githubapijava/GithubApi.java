5478499d93ca38b7dfc75bd49a629d04aff28201
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-09-08 19:40:21 -0500

    feat(app): add Gson to parser response result to list of repos

--- app/src/main/java/code/diegohdez/githubapijava/GithubApi.java
+++ app/src/main/java/code/diegohdez/githubapijava/GithubApi.java
@@ -3,19 +3,27 @@ package code.diegohdez.githubapijava;
 import android.app.Application;
 
 import com.androidnetworking.AndroidNetworking;
-import com.jacksonandroidnetworking.JacksonParserFactory;
+import com.androidnetworking.gsonparserfactory.GsonParserFactory;
+import com.facebook.stetho.okhttp3.StethoInterceptor;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import code.diegohdez.githubapijava.Manager.AppManager;
 import code.diegohdez.githubapijava.Migration.Migration;
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
+import okhttp3.OkHttpClient;
 
 public class GithubApi extends Application{
     @Override
     public void onCreate() {
         super.onCreate();
-        AndroidNetworking.initialize(getApplicationContext());
-        AndroidNetworking.setParserFactory(new JacksonParserFactory());
+        OkHttpClient okHttpClient = new OkHttpClient() .newBuilder()
+                .addNetworkInterceptor(new StethoInterceptor())
+                .build();
+        AndroidNetworking.initialize(getApplicationContext(), okHttpClient);
+        Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").create();
+        AndroidNetworking.setParserFactory(new GsonParserFactory(gson));
         Realm.init(this);
         RealmConfiguration realmConfiguration = new RealmConfiguration
                 .Builder()
