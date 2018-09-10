3f05f66c551c84dc8046f089b4e000b955dda905
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-09-10 12:38:46 -0500

    feat(ReposActivty): create recycler view to display repos info

--- app/src/main/java/code/diegohdez/githubapijava/Activity/ReposActivity.java
+++ app/src/main/java/code/diegohdez/githubapijava/Activity/ReposActivity.java
@@ -2,18 +2,41 @@ package code.diegohdez.githubapijava.Activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import java.util.ArrayList;
+
+import code.diegohdez.githubapijava.Adapter.ReposAdapter;
+import code.diegohdez.githubapijava.Data.DataOfRepos;
+import code.diegohdez.githubapijava.Manager.AppManager;
+import code.diegohdez.githubapijava.Model.Repo;
 import code.diegohdez.githubapijava.R;
 import io.realm.Realm;
+import io.realm.RealmResults;
 
 public class ReposActivity extends AppCompatActivity {
 
     private Realm realm;
+    private AppManager appManager;
+    private String account;
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private RecyclerView.Adapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_repos);
         realm = Realm.getDefaultInstance();
+        account = AppManager.getOurInstance().getAccount();
+        recyclerView = findViewById(R.id.reposList);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        RealmResults<Repo> repos = realm.where(Repo.class).equalTo("owner.login", account).findAll();
+        ArrayList<DataOfRepos> list = DataOfRepos.createRepoList(repos);
+        adapter = new ReposAdapter(list, account);
+        recyclerView.setAdapter(adapter);
     }
 
     @Override
