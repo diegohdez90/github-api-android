caec21c2b6a3f579de83945f7cb45cb9747742cd
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-09-13 20:19:39 -0500

    feat(Repos): add async task to call endpoint from account summary

--- app/src/main/java/code/diegohdez/githubapijava/AsyncTask/Repos.java
+++ app/src/main/java/code/diegohdez/githubapijava/AsyncTask/Repos.java
@@ -2,22 +2,26 @@ package code.diegohdez.githubapijava.AsyncTask;
 
 import android.content.Context;
 import android.os.AsyncTask;
+import android.util.Log;
 
 import com.androidnetworking.common.ANRequest;
 import com.androidnetworking.common.ANResponse;
 import com.androidnetworking.error.ANError;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import code.diegohdez.githubapijava.Activity.MainActivity;
 import code.diegohdez.githubapijava.Activity.ReposActivity;
 import code.diegohdez.githubapijava.Manager.AppManager;
+import code.diegohdez.githubapijava.Model.Owner;
 import code.diegohdez.githubapijava.Model.Repo;
 import code.diegohdez.githubapijava.Utils.Request.API;
 import io.realm.Realm;
 
-public class Repos extends AsyncTask<String, Void, ANResponse<List<Repo>>> {
+public class Repos extends AsyncTask<String, Void, ANResponse[]> {
 
+    public static final String TAG = Repos.class.getSimpleName();
     private Realm realm;
     private Context context;
     API api;
@@ -42,42 +46,84 @@ public class Repos extends AsyncTask<String, Void, ANResponse<List<Repo>>> {
     }
 
     @Override
-    protected ANResponse<List<Repo>> doInBackground(String... urls) {
-        ANRequest request = API.getRepos(urls[0], page);
-        return (ANResponse<List<Repo>>) request.executeForObjectList(Repo.class);
+    protected ANResponse[] doInBackground(String... urls) {
+        ANRequest[] request = new ANRequest[2];
+        request[0] = API.getRepos(urls[0], page);
+        ANResponse user = null;
+        ANResponse repos = (ANResponse<List<Repo>>) request[0].executeForObjectList(Repo.class);
+        if (urls.length > 1) {
+            request[1] = API.getAccount(urls[1]);
+            user = (ANResponse<Owner>) request[1].executeForObject(Owner.class);
+        }
+        ANResponse[] results = new ANResponse[2];
+        results[0] = repos;
+        if (urls.length > 1) results[1] = user;
+        return results;
     }
 
     @Override
-    protected void onPostExecute(ANResponse<List<Repo>> response) {
+    protected void onPostExecute(ANResponse[] response) {
+
+        super.onPostExecute(response);
+        List<Repo> list = new ArrayList<>();
         String message = "";
         int status = 0;
-        super.onPostExecute(response);
-        if (response.isSuccess()) {
-            final List<Repo> repos = (List<Repo>) response.getResult();
+        ANResponse repos = response[0];
+        if (repos.isSuccess()) {
+            list = (List<Repo>) repos.getResult();
             if (realm.isClosed()) {
                 realm = Realm.getDefaultInstance();
             }
+            final List<Repo> listOfRepos = list;
             realm.executeTransaction(new Realm.Transaction() {
                 @Override
                 public void execute(Realm realm) {
-                    realm.insertOrUpdate(repos);
+                    realm.insertOrUpdate(listOfRepos);
                 }
             });
             message = AppManager.getOurInstance().getAccount() + " repositories successfully";
-            status = response.getOkHttpResponse().code();
+            status = repos.getOkHttpResponse().code();
         } else {
             AppManager.getOurInstance().resetAccount();
-            ANError anError = response.getError();
+            ANError anError = repos.getError();
             message = "Error: " + anError.getErrorDetail() + "\n" +
                     "Body: " + anError.getErrorBody() + "\n" +
                     "Message: " + anError.getMessage() + "\n" +
                     "Code: " + anError.getErrorCode();
             status = anError.getErrorCode();
         }
+        if (response[1] != null) {
+            ANResponse user = response[1];
+            if (user.isSuccess()) {
+                final Owner owner = (Owner) user.getResult();
+                if (realm.isClosed()) realm = Realm.getDefaultInstance();
+                realm.executeTransaction(new Realm.Transaction() {
+                    @Override
+                    public void execute(Realm realm) {
+                        Log.i(TAG, "Update account");
+                        realm.insertOrUpdate(owner);
+                    }
+                });
+            } else {
+                AppManager.getOurInstance().resetAccount();
+                ANError anError = user.getError();
+                message = "Error: " + anError.getErrorDetail() + "\n" +
+                        "Body: " + anError.getErrorBody() + "\n" +
+                        "Message: " + anError.getMessage() + "\n" +
+                        "Code: " + anError.getErrorCode();
+                Log.e(TAG, message);
+            }
+        }
         realm.close();
         switch (context.getClass().getSimpleName()) {
             case "MainActivity":
                 MainActivity.successRepos(context, message, status);
+                break;
+            case "ReposActivity":
+
+                ReposActivity activity = (ReposActivity) context;
+                activity.successLoader(message, status, list);
+                break;
         }
     }
 }
