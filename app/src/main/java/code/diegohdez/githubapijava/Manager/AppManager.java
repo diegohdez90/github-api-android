c2f2cbcbb247686a967f1c214caf4401fdf8204f
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-09-14 12:36:08 -0500

    feat(AppManager): add page in manager application

--- app/src/main/java/code/diegohdez/githubapijava/Manager/AppManager.java
+++ app/src/main/java/code/diegohdez/githubapijava/Manager/AppManager.java
@@ -18,6 +18,8 @@ public class AppManager {
     private String account;
     private final String TOKEN = "TOKEN";
     private String token;
+    private final String CURRENT_PAGE = "CURRENT_PAGE";
+    private int currentPage;
 
     public void setAccount (String account) {
         this.account = account;
@@ -40,6 +42,14 @@ public class AppManager {
         return token;
     }
 
+    public int getCurrentPage() {
+        return currentPage;
+    }
+
+    public void setCurrentPage(int currentPage) {
+        this.currentPage = currentPage;
+    }
+
     public static void init (Context context) {
         ourInstance = new AppManager(context.getApplicationContext());
     }
@@ -49,14 +59,18 @@ public class AppManager {
         editor = prefs.edit();
         account = prefs.getString(ACCOUNT, "");
         token = prefs.getString(TOKEN, "");
+        currentPage = prefs.getInt(CURRENT_PAGE, 1);
     }
 
     public void resetAccount () {
         setAccount("");
     }
 
+    public void initPager () { setCurrentPage(1); }
+
     public void logout () {
         setAccount("");
         setToken("");
+        setCurrentPage(1);
     }
 }
