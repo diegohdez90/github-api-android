e38997230ca4c81d7dd8afa62ca31a72d7652bd7
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-09-14 12:39:10 -0500

    chore(API): add constants for watch and star

--- app/src/main/java/code/diegohdez/githubapijava/Utils/Constants/API.java
+++ app/src/main/java/code/diegohdez/githubapijava/Utils/Constants/API.java
@@ -6,10 +6,16 @@ public class API {
     public static final String REPOS = "/repos";
     public static final String USER_REPOS = "repos/";
     public static final String USER = "user";
-    public static final String WATCH = "/subscription";
-    public static final String STAR = "/starred";
+    public static final String WATCH_REPO = "/subscription";
+    public static final String STAR_REPO = "/starred";
 
     public static final int PAGE_SIZE = 30;
+
+    public static final String UN_WATCH = "Unwatch";
+    public static final String UN_STAR = "Unstar";
+
+    public static final String WATCH = "Watch";
+    public static final String STAR = "Star";
 
     public static final String GET_TOKEN = BASE_URL + USER;
 
