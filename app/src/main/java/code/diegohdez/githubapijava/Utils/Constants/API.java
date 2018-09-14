31fa7fe12b4ad5323eb9cd9fc1a6a0fcc1de2103
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-09-13 20:21:57 -0500

    chore(API): add page size default value

--- app/src/main/java/code/diegohdez/githubapijava/Utils/Constants/API.java
+++ app/src/main/java/code/diegohdez/githubapijava/Utils/Constants/API.java
@@ -4,7 +4,12 @@ public class API {
     public static final String BASE_URL = "https://api.github.com/";
     public static final String USERS = "users/";
     public static final String REPOS = "/repos";
+    public static final String USER_REPOS = "repos/";
     public static final String USER = "user";
+    public static final String WATCH = "/subscription";
+    public static final String STAR = "/starred";
+
+    public static final int PAGE_SIZE = 30;
 
     public static final String GET_TOKEN = BASE_URL + USER;
 
