b86c9372081be475f7f1c22f39daaa8ceddfab0e
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-09-15 17:33:17 -0500

    chore(Fields): add old and new fields to added in Migration file

--- app/src/main/java/code/diegohdez/githubapijava/Utils/Constants/Fields.java
+++ app/src/main/java/code/diegohdez/githubapijava/Utils/Constants/Fields.java
@@ -2,6 +2,22 @@ package code.diegohdez.githubapijava.Utils.Constants;
 
 public class Fields {
     public static final String OWNER_LOGIN = "owner.login";
+    public static final String ID ="id";
     public static final String LOGIN = "login";
     public static final String REPO_NAME = "name";
+    public static final String OWNER = "owner";
+    public static final String STARS = "stars";
+    public static final String WATCHERS = "watchers";
+    public static final String FORKS = "forks";
+    public static final String SUBSCRIBERS = "subscribers";
+    public static final String OLD_FULL_NAME = "full_name";
+    public static final String OLD_CREATED_AT = "created_at";
+    public static final String OLD_PUSHED_AT = "pushed_at";
+    public static final String OLD_UPDATED_AT = "updated_at";
+    public static final String FULL_NAME = "fullName";
+    public static final String CREATED_AT = "createdAt";
+    public static final String PUSHED_AT = "pushedAt";
+    public static final String UPDATED_AT = "updatedAt";
+    public static final String REPOS = "repos";
+
 }
