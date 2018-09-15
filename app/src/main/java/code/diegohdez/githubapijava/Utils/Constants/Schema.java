54dd56003118493ca42a9e88d33d149904a4d97d
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-09-15 17:33:45 -0500

    chore(Schena): init schema constants values

---
+++ app/src/main/java/code/diegohdez/githubapijava/Utils/Constants/Schema.java
@@ -0,0 +1,6 @@
+package code.diegohdez.githubapijava.Utils.Constants;
+
+public class Schema {
+    public static final String REPO_SCHEMA = "Repo";
+    public static final String OWNER_SCHEMA = "Owner";
+}
