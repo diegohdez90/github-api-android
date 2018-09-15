25d58506aa945b88065b9539effe0123ecaf559c
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-09-15 16:40:11 -0500

    chore(Constants): init repo activity string constants

---
+++ app/src/main/java/code/diegohdez/githubapijava/Utils/Constants/Dialog.java
@@ -0,0 +1,5 @@
+package code.diegohdez.githubapijava.Utils.Constants;
+
+public class Dialog {
+    public static final String CLOSE_REPO_DETAIL_DIALOG = "Close";
+}
---
+++ app/src/main/java/code/diegohdez/githubapijava/Utils/Constants/Fields.java
@@ -0,0 +1,7 @@
+package code.diegohdez.githubapijava.Utils.Constants;
+
+public class Fields {
+    public static final String OWNER_LOGIN = "owner.login";
+    public static final String LOGIN = "login";
+    public static final String REPO_NAME = "name";
+}
