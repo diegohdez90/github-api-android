15b8bbcceb66862612c513ee8a5067d55d4b4c9d
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-09-17 13:40:08 -0500

    feat(app): upgrade db version

--- app/src/main/java/code/diegohdez/githubapijava/GithubApi.java
+++ app/src/main/java/code/diegohdez/githubapijava/GithubApi.java
@@ -28,7 +28,7 @@ public class GithubApi extends Application{
         RealmConfiguration realmConfiguration = new RealmConfiguration
                 .Builder()
                 .name("github-api")
-                .schemaVersion(5)
+                .schemaVersion(6)
                 .migration(new Migration())
                 .build();
         Realm.setDefaultConfiguration(realmConfiguration);
