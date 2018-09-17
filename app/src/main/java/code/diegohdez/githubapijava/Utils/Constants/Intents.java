805c5b92ea7e98af5a8672d6aa69ef0397859cb3
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-09-17 13:46:04 -0500

    chore(Intents): add intents values for repos detail activity

---
+++ app/src/main/java/code/diegohdez/githubapijava/Utils/Constants/Intents.java
@@ -0,0 +1,6 @@
+package code.diegohdez.githubapijava.Utils.Constants;
+
+public class Intents {
+    public static final String REPO_ID = "REPO_ID";
+    public static final String REPO_NAME = "REPO_NAME";
+}
