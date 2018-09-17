28c30b95d940ec7a35221cff9933f921701fc72f
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-09-17 13:38:57 -0500

    chore(Schema): add Issue schema def

--- app/src/main/java/code/diegohdez/githubapijava/Utils/Constants/Schema.java
+++ app/src/main/java/code/diegohdez/githubapijava/Utils/Constants/Schema.java
@@ -3,4 +3,5 @@ package code.diegohdez.githubapijava.Utils.Constants;
 public class Schema {
     public static final String REPO_SCHEMA = "Repo";
     public static final String OWNER_SCHEMA = "Owner";
+    public static final String ISSUE_SCHEMA = "Issue";
 }
