f19a06cfbaa173ca550b79f8c8d0167e4029b2d4
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-09-17 19:52:16 -0500

    feat(IssuesRepo): add method to parse as RealmList to add into repo selected

--- app/src/main/java/code/diegohdez/githubapijava/AsyncTask/IssuesRepo.java
+++ app/src/main/java/code/diegohdez/githubapijava/AsyncTask/IssuesRepo.java
@@ -8,6 +8,8 @@ import com.androidnetworking.common.ANRequest;
 import com.androidnetworking.common.ANResponse;
 import com.androidnetworking.error.ANError;
 
+import java.util.ArrayList;
+
 import code.diegohdez.githubapijava.Activity.ReposDetailActivity;
 import code.diegohdez.githubapijava.Model.Issue;
 import code.diegohdez.githubapijava.Model.Repo;
@@ -33,24 +35,25 @@ public class IssuesRepo extends AsyncTask<String, Void, ANResponse>{
     @Override
     protected ANResponse doInBackground(String... urls) {
         ANRequest request = API.getIssues(urls[0]);
-        return (ANResponse<IssuesRepo>) request.executeForObjectList(IssuesRepo.class);
+        return (ANResponse) request.executeForObjectList(Issue.class);
     }
 
     @Override
     protected void onPostExecute(ANResponse response) {
         super.onPostExecute(response);
         if (response.isSuccess()) {
-            final RealmList<Issue> list = (RealmList<Issue>) response.getResult();
             if (realm.isClosed()) realm = Realm.getDefaultInstance();
+            final RealmList<Issue> list = toIssueList(realm, (ArrayList<Issue>) response.getResult());
             realm.executeTransaction(new Realm.Transaction() {
                 @Override
                 public void execute(Realm realm) {
                     Repo repo = realm.where(Repo.class).equalTo(Fields.ID, id).findFirst();
-                    repo.setIssues(list);
+                    repo.getIssues().addAll(list);
                     realm.insertOrUpdate(repo);
                 }
             });
             realm.close();
+            Log.i(TAG, "create issues : " + id);
             ((ReposDetailActivity) context).createIssuesAdapter(id);
         } else {
             ANError anError = response.getError();
@@ -61,5 +64,24 @@ public class IssuesRepo extends AsyncTask<String, Void, ANResponse>{
                     "Code: " + anError.getErrorCode();
             Log.e(TAG, message);
         }
+    }
+
+    private RealmList<Issue> toIssueList(Realm realm, ArrayList<Issue> list) {
+        RealmList<Issue> issues = new RealmList();
+        for (Issue item: list) {
+            Issue issue = new Issue();
+            issue.setId(item.getId());
+            issue.setTitle(item.getTitle());
+            issue.setDescription(item.getDescription());
+            issue.setNumber(item.getNumber());
+            issue.setState(item.getState());
+            issue.setUser(item.getUser());
+            if (item.getAssignee() != null) issue.setAssignee(item.getAssignee());
+            issue.setCreatedAt(item.getCreatedAt());
+            issue.setUpdatedAt(item.getUpdatedAt());
+            if (issue.getState() != "closed") issue.setClosedAt(item.getClosedAt());
+            issues.add(issue);
+        }
+        return issues;
     }
 }
