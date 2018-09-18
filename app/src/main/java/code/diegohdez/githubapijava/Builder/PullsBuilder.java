d98a17850bb66e6b1015471e9b3cb4304f6ccf2b
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-09-18 16:22:47 -0500

    feat(DataOfPulls): add builder to get pulls object to display in recycler view

---
+++ app/src/main/java/code/diegohdez/githubapijava/Builder/PullsBuilder.java
@@ -0,0 +1,125 @@
+package code.diegohdez.githubapijava.Builder;
+
+import java.util.Date;
+
+import code.diegohdez.githubapijava.Data.DataOfPulls;
+import code.diegohdez.githubapijava.Model.Owner;
+
+import static code.diegohdez.githubapijava.Utils.Constants.API.CLOSED;
+
+public class PullsBuilder {
+
+    private long id;
+    private String title;
+    private String description;
+    private long number;
+    private String state;
+    private String user;
+    private String assignee;
+    private Date closedAt;
+    private Date createdAt;
+    private Date updatedAt;
+    private Date mergedAt;
+
+    public PullsBuilder (long id) {
+        this.id = id;
+    }
+
+    public PullsBuilder setTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public PullsBuilder setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public PullsBuilder setNumber(long number) {
+        this.number = number;
+        return this;
+    }
+
+    public PullsBuilder setState(String state) {
+        this.state = state;
+        return this;
+    }
+
+    public PullsBuilder setUser(Owner user) {
+        this.user = user.getLogin();
+        return this;
+    }
+
+    public PullsBuilder setAssignee(Owner assignee) {
+        if (assignee != null) this.assignee = assignee.getLogin();
+        return this;
+    }
+
+    public PullsBuilder setCreatedAt(Date createdAt) {
+        this.createdAt =createdAt;
+        return this;
+    }
+
+    public PullsBuilder setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+        return this;
+    }
+
+    public PullsBuilder setClosedAt(Date closedAt) {
+        if (this.state == CLOSED) this.closedAt = closedAt;
+        return this;
+    }
+
+    public PullsBuilder setMergedAt(Date mergedAt) {
+        if (mergedAt != null) this.mergedAt = mergedAt;
+            return this;
+    }
+
+    public DataOfPulls build(){
+        return new DataOfPulls(this);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public long getNumber() {
+        return number;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getAssignee() {
+        return assignee;
+    }
+
+    public Date getClosedAt() {
+        return closedAt;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public Date getMergedAt() {
+        return mergedAt;
+    }
+}
---
+++ app/src/main/java/code/diegohdez/githubapijava/Data/DataOfPulls.java
@@ -0,0 +1,101 @@
+package code.diegohdez.githubapijava.Data;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import code.diegohdez.githubapijava.Builder.PullsBuilder;
+import code.diegohdez.githubapijava.Model.Pull;
+
+public class DataOfPulls {
+
+    private long id;
+    private String title;
+    private String description;
+    private long number;
+    private String state;
+    private String user;
+    private String assignee;
+    private Date closedAt;
+    private Date createdAt;
+    private Date updatedAt;
+    private Date mergedAt;
+
+    public DataOfPulls(PullsBuilder builder) {
+        this.id = builder.getId();
+        this.title = builder.getTitle();
+        this.description = builder.getDescription();
+        this.number = builder.getNumber();
+        this.state = builder.getState();
+        this.user = builder.getUser();
+        this.assignee = builder.getAssignee();
+        this.createdAt = builder.getCreatedAt();
+        this.updatedAt = builder.getUpdatedAt();
+        this.closedAt = builder.getClosedAt();
+        this.mergedAt = builder.getMergedAt();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public long getNumber() {
+        return number;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getAssignee() {
+        return assignee;
+    }
+
+    public Date getClosedAt() {
+        return closedAt;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public Date getMergedAt() {
+        return mergedAt;
+    }
+
+    public static ArrayList<DataOfPulls> createList(List<Pull> pulls) {
+        ArrayList<DataOfPulls> list = new ArrayList<>();
+        for (Pull pull : pulls) {
+            DataOfPulls item = new PullsBuilder(pull.getId())
+                    .setTitle(pull.getTitle())
+                    .setDescription(pull.getDescription())
+                    .setNumber(pull.getNumber())
+                    .setState(pull.getState())
+                    .setUser(pull.getUser())
+                    .setAssignee(pull.getAssignee())
+                    .setCreatedAt(pull.getCreatedAt())
+                    .setUpdatedAt(pull.getUpdatedAt())
+                    .setClosedAt(pull.getClosedAt())
+                    .setMergedAt(pull.getMergedAt())
+                    .build();
+            list.add(item);
+        }
+        return list;
+    }
+}
