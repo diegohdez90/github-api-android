197dfdf1a96222f73f8b3fc0bb46fe564d7012db
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-09-17 19:45:35 -0500

    feat(IssuesFragmentAdapter): update fragment, add recycler view and adapter to update issues list view

--- app/src/main/java/code/diegohdez/githubapijava/Adapter/IssuesFragmentAdapter.java
+++ app/src/main/java/code/diegohdez/githubapijava/Adapter/IssuesFragmentAdapter.java
@@ -5,21 +5,28 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
+import java.util.ArrayList;
+
+import code.diegohdez.githubapijava.Data.DataOfIssues;
 import code.diegohdez.githubapijava.R;
 
 @SuppressLint("ValidFragment")
-public class IssuesAdapter extends Fragment {
+public class IssuesFragmentAdapter extends Fragment implements UpdateableFragment {
 
     public static final String ISSUE_TITLE = "ISSUE_TITLE";
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    private ArrayList<DataOfIssues> issues;
+    RecyclerView recyclerView;
+    IssuesAdapter adapter;
+
+    public IssuesFragmentAdapter(ArrayList<DataOfIssues> issues) {
+        this.issues = issues;
     }
 
     @Nullable
@@ -27,10 +34,20 @@ public class IssuesAdapter extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.item_issue, container, false);
-        Bundle args = getArguments();
-        ((TextView) root.findViewById(R.id.issue_title))
-                .setText(args.getString(ISSUE_TITLE));
+        View root = inflater.inflate(R.layout.issues_fragment, container, false);
+
+        recyclerView = (RecyclerView) root.findViewById(R.id.issues_list);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        adapter = new IssuesAdapter();
+        recyclerView.setScrollbarFadingEnabled(true);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
         return  root;
+    }
+
+    @Override
+    public void update(ArrayList<DataOfIssues> issues) {
+        adapter.addIssues(issues);
     }
 }
