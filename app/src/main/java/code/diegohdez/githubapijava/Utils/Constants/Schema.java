fffd459d346fda4c82069c4b5954e4e96c8ccd89
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-09-18 11:57:27 -0500

    chore(Schema): add Pull and PullInfo name schema

--- app/src/main/java/code/diegohdez/githubapijava/Utils/Constants/Schema.java
+++ app/src/main/java/code/diegohdez/githubapijava/Utils/Constants/Schema.java
@@ -4,4 +4,6 @@ public class Schema {
     public static final String REPO_SCHEMA = "Repo";
     public static final String OWNER_SCHEMA = "Owner";
     public static final String ISSUE_SCHEMA = "Issue";
+    public static final String PULL_SCHEMA = "Pull";
+    public static final String PULL_INFO_SCHEMA = "PullInfo";
 }
