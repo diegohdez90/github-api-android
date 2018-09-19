199e5027f81ae44ac7ea77c2d87298a632a89efe
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-09-18 19:37:18 -0500

    feat(IssuesFragment): add scroll paginate listener to get issues from next page
    add issues in adapter
    
    add id and repo name as arguments

--- app/src/main/java/code/diegohdez/githubapijava/Adapter/IssuesFragmentAdapter.java
+++ app/src/main/java/code/diegohdez/githubapijava/Adapter/IssuesFragmentAdapter.java
@@ -10,18 +10,35 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
+import code.diegohdez.githubapijava.AsyncTask.IssuesRepo;
 import code.diegohdez.githubapijava.Data.DataOfIssues;
+import code.diegohdez.githubapijava.Manager.AppManager;
 import code.diegohdez.githubapijava.R;
+import code.diegohdez.githubapijava.ScrollListener.IssuesPaginationScrollListener;
+
+import static code.diegohdez.githubapijava.Utils.Constants.API.BASE_URL;
+import static code.diegohdez.githubapijava.Utils.Constants.API.PAGE_SIZE;
+import static code.diegohdez.githubapijava.Utils.Constants.API.STATE_ALL;
+import static code.diegohdez.githubapijava.Utils.Constants.API.USER_ISSUES;
+import static code.diegohdez.githubapijava.Utils.Constants.API.USER_REPOS;
+import static code.diegohdez.githubapijava.Utils.Constants.Numbers.PAGE_ONE;
 
 @SuppressLint("ValidFragment")
 public class IssuesFragmentAdapter extends Fragment implements UpdateableIssuesFragment {
 
+    public static final String ARG_ID = "ID";
+    public static final String ARG_REPO_NAME = "REPO_NAME";
+
     RecyclerView recyclerView;
     IssuesAdapter adapter;
 
+    int page = PAGE_ONE;
+    boolean isLoading = false;
+    boolean isLastPage = false;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -29,6 +46,7 @@ public class IssuesFragmentAdapter extends Fragment implements UpdateableIssuesF
                              @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.issues_fragment, container, false);
 
+        final Bundle args = getArguments();
         recyclerView = root.findViewById(R.id.issues_list);
         LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
         adapter = new IssuesAdapter();
@@ -36,11 +54,45 @@ public class IssuesFragmentAdapter extends Fragment implements UpdateableIssuesF
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adapter);
+        recyclerView.addOnScrollListener( new IssuesPaginationScrollListener(layoutManager) {
+
+            @Override
+            protected void loadIssues() {
+                isLoading = true;
+                page++;
+                Toast.makeText(
+                        getContext(),
+                        "Display page " + page,
+                        Toast.LENGTH_SHORT).show();
+                IssuesRepo loadIssues = new IssuesRepo(getActivity(), args.getLong(ARG_ID));
+                loadIssues.execute(BASE_URL +
+                        USER_REPOS +
+                        AppManager.getOurInstance().getAccount() +
+                        "/" +args.getString(ARG_REPO_NAME) +
+                        USER_ISSUES + STATE_ALL + "&page=" + page);
+            }
+
+            @Override
+            public boolean isLastPage() {
+                return isLastPage;
+            }
+
+            @Override
+            public boolean isLoading() {
+                return isLoading;
+            }
+        });
         return  root;
     }
 
     @Override
     public void update(ArrayList<DataOfIssues> issues) {
+        if (page > PAGE_ONE) {
+            adapter.deleteLoading();
+            isLoading = false;
+        }
         adapter.addIssues(issues);
+        if (issues.size() < PAGE_SIZE) isLastPage = true;
+        else adapter.addLoading();
     }
 }
