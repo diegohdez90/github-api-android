904f28ebf5633e5270572bb2272ea1f4db853f92
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-09-19 14:59:30 -0500

    chore(Intents): add branch name intent

--- app/src/main/java/code/diegohdez/githubapijava/Utils/Constants/Intents.java
+++ app/src/main/java/code/diegohdez/githubapijava/Utils/Constants/Intents.java
@@ -3,4 +3,5 @@ package code.diegohdez.githubapijava.Utils.Constants;
 public class Intents {
     public static final String REPO_ID = "REPO_ID";
     public static final String REPO_NAME = "REPO_NAME";
+    public static final String BRANCH_NAME = "BRANCH_NAME";
 }
